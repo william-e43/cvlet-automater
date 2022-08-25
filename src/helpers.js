d2515d0a031b3ce6a5f36fe1bd9257210158ef4e
William <williameliason8@gmail.com>
2022-08-25 12:20:48 -0500

    update package json

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-import pdfMake from "pdfmake/build/pdfmake";
+import * as pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
